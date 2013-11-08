cf252939c52b834e3af4be4eec7ec0e25588dec7
jackerman09 <jackerman09@gmail.com>
2013-11-08 14:02:04 -0500

    add devise and static_pages controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ WhatKeyV003::Application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
+  # Added when installing devise
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 WhatKeyV003::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
+  get "static_pages/home"
+  devise_for :users
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
