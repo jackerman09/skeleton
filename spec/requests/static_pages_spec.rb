0cf333da9ec376a4264f7f6a48fa9a2d4258e2f7
jackerman09 <jackerman09@gmail.com>
2013-12-10 14:03:35 -0500

    update header with chords/keys/notes links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,29 +5,38 @@ describe "StaticPages" do
 
   describe "home" do
     before { visit root_path }
-    it { should have_content('Skeleton App') }
+    
+    it { should have_content('What Key Am I In?') }
     it { should have_link('Sign up') }
-  end
+    it { should have_link('Log in') }
 
-  describe "as logged in user" do
-  	before do
-  		@user = FactoryGirl.create(:user)
-  		@user.confirm!
-  		test_sign_in(@user)
-  	end
-  	it { should have_content('Skeleton App') }
-  	it { should have_content('User Profile') }
-  	it { should have_content('You cannot edit notes') }
-  end
+    it { should have_link('All Keys') }
+    it { should have_link('All Chords') }
 
-  describe "as admin" do
-  	before do
-  		admin = FactoryGirl.create(:admin)
-  		admin.confirm!
-  		test_sign_in(admin)
-  	end
-  	it { should have_content('Skeleton App') }
-  	it { should have_content('User Profile') }
-  	it { should have_content('You can edit notes') }
+    describe "as logged in user" do
+    	before do
+    		@user = FactoryGirl.create(:user)
+    		@user.confirm!
+    		test_sign_in(@user)
+    	end
+    	it { should have_content('What Key Am I In?') }
+    	it { should have_content(@user.username) }
+    	it { should have_content('You cannot edit notes') }
+
+      it { should_not have_content('All Notes') }
+    end
+
+    describe "as admin" do
+    	before do
+    		@admin = FactoryGirl.create(:admin)
+    		@admin.confirm!
+    		test_sign_in(@admin)
+    	end
+    	it { should have_content('What Key Am I In?') }
+    	it { should have_content(@admin.username) }
+    	it { should have_content('You can edit notes') }
+
+      it { should have_link('All Notes') }
+    end
   end
 end
