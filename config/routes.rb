828f9144cd04c80f456d8fb30fcc2c40d5615b53
jackerman09 <jackerman09@gmail.com>
2013-12-10 13:48:00 -0500

    add about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 WhatKeyV003::Application.routes.draw do
   resources :notes
-  get "static_pages/home"
   devise_for :users
   resources :users
 
   root 'static_pages#home'
+
+  get "static_pages/about"
+  get "static_pages/contact"
 end
