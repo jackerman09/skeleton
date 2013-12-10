8e2df05de0c3ead4c5e2fa58000120134483b3da
jackerman09 <jackerman09@gmail.com>
2013-12-10 14:26:37 -0500

    add static_pages_presenter.rb but it doesn't currently do anything

---
+++ app/presenters/static_pages_presenter.rb
@@ -0,0 +1,17 @@
+class StaticPagesPresenter < BasePresenter
+  # presents :static_pages
+  # delegate :username, to: :user
+
+	def h
+		@template
+	end
+
+	def admin_text
+		if user.admin?
+			'This is an admin'
+			# h.link_to root_path
+		else
+			'This is not an admin'
+		end
+	end
+end
